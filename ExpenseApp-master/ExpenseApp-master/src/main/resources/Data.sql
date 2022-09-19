907bbdc66c0f439dd0964943df36c99465607c08
Kartikey Rai <63674571+Kartik-r@users.noreply.github.com>
2022-09-19 17:15:38 +0530

    Update Data.sql

--- ExpenseApp-master/ExpenseApp-master/src/main/resources/Data.sql
+++ ExpenseApp-master/ExpenseApp-master/src/main/resources/Data.sql
@@ -1,16 +1,16 @@
 
-insert into user values (1,'Siamak', 'Codeengine11@gmail.com')
-insert into user values (2,'John', 'John@john.com')
-insert into user values (3,'Adam', 'adam@adam.com')
+insert into user values (1,'Kartik', 'kartik11@gmail.com')
+insert into user values (2,'Rohan', 'Rohan@gmail.com')
+insert into user values (3,'Amit', 'amit@gmail.com')
 
 
 insert into category values (1,'Travel')
-insert into category values (2,'Auto Loan')
-insert into category  values (3,'Travel')
+insert into category values (2,'Shopping')
+insert into category  values (3,'Food')
 
 
-insert into expense values (100,'New York Business Trip','2019-06-16T17:00:00.000Z','New York',1,1)
-insert into expense values (101,'Ford Mustang Payment','2019-06-15T15:00:00.000Z','Los Angeles',2,2)
-insert into expense values(102,'Grand Canyon Trip With Family','2019-06-15T15:00:00.000Z','Arizona',3,1)
+insert into expense values (100,'Mysore Trip','2022-09-1T17:00:00.000Z','Bangalore',1,1)
+insert into expense values (101,'Zudio','2022-09-15T15:00:00.000Z','Delhi',2,2)
+insert into expense values(102,'Star Bucks','2022-09-19T15:00:00.000Z','Mumbai',3,1)
  
 
